136fa4a460ceb72b5ea4e7493c66e7bad919a46e
Md-Jahid-Hasan <jahid15-1905@diu.edu.bd>
2024-06-01 00:49:26 +0600

    add search featutre in categories dropdown

--- ast_to_know_frontend/src/app/components/user-home/user-home.component.ts
+++ ast_to_know_frontend/src/app/components/user-home/user-home.component.ts
@@ -1,26 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserQuestionsComponent} from "../user-questions/user-questions.component";
 import {UserHomeSidepanelComponent} from "../user-home-sidepanel/user-home-sidepanel.component";
 import {UserHomeService} from "../../services/user-home.service";
 import {FormsModule} from "@angular/forms";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
-  selector: 'app-user-home',
-  standalone: true,
+    selector: 'app-user-home',
+    standalone: true,
     imports: [
         UserQuestionsComponent,
         UserHomeSidepanelComponent,
         FormsModule,
-        NgForOf
+        NgForOf,
+        NgIf
     ],
-  templateUrl: './user-home.component.html',
-  styleUrl: './user-home.component.css'
+    templateUrl: './user-home.component.html',
+    styleUrl: './user-home.component.css'
 })
 export class UserHomeComponent implements OnInit {
     categories: any[] = []
-    selected_category: string|null = null
-    question:{question:string, category: number|null} = {
+    searched_categories: any[] = []
+    selected_category: string | null = null
+    question: { question: string, category: number | null } = {
         question: "test",
         category: null
     }
@@ -30,25 +32,34 @@ export class UserHomeComponent implements OnInit {
 
     ngOnInit() {
         if (typeof window !== 'undefined') {
-            console.log("testeing")
-            this.user_home.getAllCategory().subscribe(categories => this.categories = categories)
+            this.user_home.getAllCategory().subscribe(categories => {
+                this.categories = categories
+                this.searched_categories = categories
+            })
         }
     }
 
-    selectCategory(category: {id:number, name:string}) {
+    selectCategory(category: { id: number, name: string }) {
         this.question.category = category.id
         this.selected_category = category.name
     }
 
+    searchCategory(event: any) {
+        let query = event.target.value
+        if (query=="") this.searched_categories = this.categories
+        else this.searched_categories = this.categories.filter(cat => cat.name.toLowerCase().includes(query))
+    }
+
     submitQuestion() {
-        if (typeof window !== 'undefined' && window.localStorage) {
+        if (typeof window !== 'undefined' && window.localStorage &&
+            this.question.category != null && this.question.question != "") {
             this.user_home.createQuestion(this.question).subscribe(
                 response => {
-                    console.log(response)
                     this.question = {
                         question: "",
                         category: null
                     }
+                    this.selected_category = null
                 })
         }
     }
