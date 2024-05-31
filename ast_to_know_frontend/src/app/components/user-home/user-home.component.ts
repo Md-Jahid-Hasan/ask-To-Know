0b265bf9ef62f07aeedde3759b56483a892dcd72
Md-Jahid-Hasan <jahid15-1905@diu.edu.bd>
2024-05-31 11:37:31 +0600

    user home page updated

--- ast_to_know_frontend/src/app/components/user-home/user-home.component.ts
+++ ast_to_know_frontend/src/app/components/user-home/user-home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {UserQuestionsComponent} from "../user-questions/user-questions.component";
+import {UserHomeSidepanelComponent} from "../user-home-sidepanel/user-home-sidepanel.component";
 
 @Component({
   selector: 'app-user-home',
   standalone: true,
-  imports: [],
+    imports: [
+        UserQuestionsComponent,
+        UserHomeSidepanelComponent
+    ],
   templateUrl: './user-home.component.html',
   styleUrl: './user-home.component.css'
 })
