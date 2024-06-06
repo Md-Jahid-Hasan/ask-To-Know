9fa5090c7a1acbc8e44c3bc1c638bd5cea67c363
Md-Jahid-Hasan <jahid15-1905@diu.edu.bd>
2024-06-07 00:28:40 +0600

    some feature updated

--- ast_to_know_frontend/src/app/app.routes.ts
+++ ast_to_know_frontend/src/app/app.routes.ts
@@ -4,8 +4,6 @@ import {UserCreateComponent} from "./components/user-create/user-create.componen
 import {AgentHomeComponent} from "./components/agent-home/agent-home.component";
 import {UserHomeComponent} from "./components/user-home/user-home.component";
 import {AgentTaskComponent} from "./components/agent-task/agent-task.component";
-import {UserQuestionsComponent} from "./components/user-questions/user-questions.component";
-import {UserQuestionDetailsComponent} from "./components/user-question-details/user-question-details.component";
 
 export const routes: Routes = [
     {path: '', component: UserHomeComponent},
@@ -13,6 +11,4 @@ export const routes: Routes = [
     {path: 'signup', component: UserCreateComponent},
     {path: 'agent', component: AgentHomeComponent},
     {path: 'task/:id', component: AgentTaskComponent},
-    {path: 'questions', component: UserQuestionsComponent},
-    {path: 'question/:id', component: UserQuestionDetailsComponent},
 ];
