e499d4e8bde51772539baeb72d534f6f19ccf713
jahid4 <jahid@impelitsolutions.com>
2024-06-15 14:58:55 +0600

    add ng-zorro active inactive button

--- ast_to_know_frontend/src/app/components/agent-home/agent-home.component.ts
+++ ast_to_know_frontend/src/app/components/agent-home/agent-home.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {PaginationComponent} from "../pagination/pagination.component";
+import {NzSwitchModule} from 'ng-zorro-antd/switch';
+import {FormsModule} from "@angular/forms";
+import {NzMessageService} from 'ng-zorro-antd/message';
+import {NzPopconfirmDirective, NzPopconfirmModule} from 'ng-zorro-antd/popconfirm';
 
 @Component({
-  selector: 'app-agent-home',
-  standalone: true,
+    selector: 'app-agent-home',
+    standalone: true,
     imports: [
-        PaginationComponent
+        PaginationComponent, NzSwitchModule, FormsModule, NzPopconfirmDirective
     ],
-  templateUrl: './agent-home.component.html',
-  styleUrl: './agent-home.component.css'
+    templateUrl: './agent-home.component.html',
+    styleUrl: './agent-home.component.css'
 })
 export class AgentHomeComponent {
+    agent_status:boolean = true;
+    status_loading: boolean = false;
 
+    cancel(): void {
+        this.status_loading = false
+        this.nzMessageService.info('click cancel', {nzAnimate: true });
+    }
+
+    confirm(): void {
+        this.agent_status = !this.agent_status
+        this.status_loading = false
+        this.nzMessageService.info('click confirm');
+    }
+
+    changingStatus(){
+        this.status_loading = true
+    }
+
+    constructor(private nzMessageService: NzMessageService) {
+    }
 }
