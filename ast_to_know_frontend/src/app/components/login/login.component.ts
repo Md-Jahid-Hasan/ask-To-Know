94a5b8b238c5e122477afab2891abb7c6960dc8f
Md-Jahid-Hasan <jahid15-1905@diu.edu.bd>
2024-07-15 00:41:15 +0600

    add error message for invalid credentials provide

--- ast_to_know_frontend/src/app/components/login/login.component.ts
+++ ast_to_know_frontend/src/app/components/login/login.component.ts
@@ -1,38 +1,49 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {Router, RouterLink} from "@angular/router";
 import {UserService} from "../../services/user.service";
+import {NzMessageService} from "ng-zorro-antd/message";
+import {NzAlertComponent} from "ng-zorro-antd/alert";
+import {NgIf} from "@angular/common";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
+    selector: 'app-login',
+    standalone: true,
     imports: [
         FormsModule,
-        RouterLink
+        RouterLink,
+        NzAlertComponent,
+        NgIf
     ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css'
 })
 export class LoginComponent {
     user_details = {
         email: "",
         password: "",
     }
+    error_message: string | null = null
 
-    constructor(private userService: UserService, private router: Router) {
+    constructor(private userService: UserService, private router: Router, private message: NzMessageService) {
     }
 
     onLogin() {
         // This method call when user press login button. This method call login api and store token in localstorage if valid.
         if (this.user_details.email == "" || this.user_details.password === "")
             alert("Valid Data not provided")
-        else{
+        else {
             this.userService.userLogin(this.user_details).subscribe(user_data => {
                     localStorage.setItem('token', user_data.access)
                     this.router.navigate(["/"])
                 },
-                error => console.log(error.error))
+                error => this.error_message = "No user found with this credentials")
         }
     }
 
+    closeErrorMessage(){
+        this.error_message = null
+    }
+
+    protected readonly console = console;
 }
