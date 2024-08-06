0aef6d8dd970527a0bf481766ad4fcdd392f9220
Md-Jahid-Hasan <jahid15-1905@diu.edu.bd>
2024-08-07 01:08:37 +0600

    create service for post feed

---
+++ ast_to_know_frontend/src/app/services/post-feed.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostFeedService {
+
+    constructor(private http: HttpClient) {}
+
+    getPostFeed(): Observable<any>{
+        return this.http.get<any>("/api/post/")
+    }
+
+    createPost(post: {content: string}):Observable<any>{
+        return this.http.post<any>("api/post/", post)
+    }
+
+    deletePost(post_id:number): Observable<any>{
+        return this.http.delete<any>(`api/post/${post_id}/`)
+    }
+
+    getCommentsOfPost(post_id: number): Observable<any>{
+        return this.http.get<any>(`api/post/comment/${post_id}/`)
+    }
+
+    createComment(post_id: number, comment: {content: string}): Observable<any>{
+        return this.http.post<any>(`api/post/comment/${post_id}/`, comment)
+    }
+
+    deleteComment(comment_id: number): Observable<any> {
+        return this.http.delete<any>(`api/post/comment/${comment_id}/`)
+    }
+
+    votePost(post_id: number, rating: {rating: string}): Observable<any> {
+        return this.http.post<any>(`api/post/vote/${post_id}/`, rating)
+    }
+}
