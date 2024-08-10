b66facb505effa6773c5d1f6591ae60b926da3a7
Md-Jahid-Hasan <jahid15-1905@diu.edu.bd>
2024-08-11 00:20:01 +0600

    add comment and delete comment api integrated and updated, add message for each action, update UI to add delete button

--- ast_to_know_frontend/src/app/components/post-feed/post-feed.component.ts
+++ ast_to_know_frontend/src/app/components/post-feed/post-feed.component.ts
@@ -13,10 +13,13 @@ import {NzAvatarComponent} from "ng-zorro-antd/avatar";
 import {NzCommentContentDirective} from "ng-zorro-antd/comment";
 
 import {PostFeedService} from "../../services/post-feed.service";
-import {Post} from "../../services/Post";
+import {Post, Comment} from "../../services/Post";
 import {getTimeDifference} from "../common/time"
 import {NzSkeletonComponent} from "ng-zorro-antd/skeleton";
 import {FormsModule} from "@angular/forms";
+import {NzDropDownDirective, NzDropdownMenuComponent} from "ng-zorro-antd/dropdown";
+import {NzMenuDirective, NzMenuItemComponent} from "ng-zorro-antd/menu";
+import {NzMessageService} from "ng-zorro-antd/message";
 
 
 @Component({
@@ -26,21 +29,22 @@ import {FormsModule} from "@angular/forms";
         NzCardComponent, NzColDirective, NzRowDirective, NzButtonComponent, NzFlexDirective, NzIconDirective,
         NzCollapseComponent, NzCollapsePanelComponent, NzRadioGroupComponent, NzRadioComponent, NgForOf,
         NzCommentComponent, NzCommentActionComponent, NzTooltipDirective, NzAvatarComponent,
-        NzCommentAvatarDirective, NzCommentContentDirective, NzSkeletonComponent, NgIf, FormsModule
+        NzCommentAvatarDirective, NzCommentContentDirective, NzSkeletonComponent, NgIf, FormsModule, NzDropDownDirective, NzDropdownMenuComponent, NzMenuDirective, NzMenuItemComponent
     ],
     templateUrl: './post-feed.component.html',
     styleUrl: './post-feed.component.css'
 })
-export class PostFeedComponent implements OnInit{
-    is_visible_vote: boolean|number = false
-    is_visible_comments: boolean|number = false
+export class PostFeedComponent implements OnInit {
+    is_visible_vote: boolean | number = false
+    is_visible_comments: boolean | number = false
     all_post: Post[] = []
-    all_comments: any = {}
+    all_comments:any = {}
     new_post: string = ""
     new_comment: string = ""
     rating: string = ""
 
-    constructor(private post_service: PostFeedService) {}
+    constructor(private post_service: PostFeedService, private messageService: NzMessageService) {
+    }
 
     ngOnInit() {
         if (typeof window !== 'undefined' && window.localStorage) {
@@ -50,22 +54,30 @@ export class PostFeedComponent implements OnInit{
         }
     }
 
-    toggleVoteView(post_id:number) {
+    toggleVoteView(post_id: number) {
         this.is_visible_vote = post_id == this.is_visible_vote ? false : post_id
     }
 
-    toggleCommentView(post_id:number){
-       this.is_visible_comments = post_id == this.is_visible_comments ? false : post_id
+    toggleCommentView(post_id: number) {
+        this.is_visible_comments = post_id == this.is_visible_comments ? false : post_id
         if (!(post_id in this.all_comments)) this.getComments(post_id)
         this.new_comment = ""
 
     }
 
-    createNewPost(){
-        this.post_service.createPost({content: this.new_post}).subscribe(post =>{
-            this.all_post.unshift(post)
-            this.new_post = ""
-        })
+    createNewPost() {
+        this.post_service.createPost({content: this.new_post}).subscribe(post => {
+                this.all_post.unshift(post)
+                this.new_post = ""
+                this.messageService.success("Successfully Posted")
+            },
+            error => {
+                let error_message = error.error
+                for (const key in error_message){
+                    this.messageService.error(`${key}: ${error_message[key][0]}`)
+                }
+
+            })
     }
 
     getComments(post_id: number) {
@@ -76,19 +88,40 @@ export class PostFeedComponent implements OnInit{
         }
     }
 
-    createComments(post_id: number){
-        if (typeof window !== 'undefined' && window.localStorage){
+    createComments(post_id: number) {
+        if (typeof window !== 'undefined' && window.localStorage) {
             this.post_service.createComment(post_id, {content: this.new_comment}).subscribe(comment => {
                 let prev_comments = this.all_comments[post_id] || []
                 prev_comments.unshift(comment)
                 this.all_comments[post_id] = prev_comments
                 this.new_comment = ""
+
+                this.all_post = this.all_post.map(post => post.id===post_id ?
+                {...post, total_comments:post.total_comments+1} : post)
             })
         }
     }
 
-    giveVote(post_id: number){
+    giveVote(post_id: number) {
+        this.post_service.votePost(post_id, {rating: this.rating}).subscribe(res => {
+            if (res.result == true) {
+                this.all_post = this.all_post.map(post => post.id == post_id ?
+                    {...post, is_voted: true, average_votes: res.average} : post)
+                this.is_visible_vote = false
+            }
+        })
+    }
 
+    deleteComment(comment_id: number, post_id: number){
+        this.post_service.deleteComment(comment_id).subscribe(res => {
+            let comments: Comment[] = this.all_comments[post_id] || []
+            comments = comments.filter(comm => comm.id !== comment_id)
+            this.all_comments[post_id] = comments
+
+            this.all_post = this.all_post.map(post => post.id===post_id ?
+                {...post, total_comments:post.total_comments-1} : post)
+            this.messageService.success("successfully deleted your message")
+        })
     }
 
     protected readonly getTimeDifference = getTimeDifference;
--- ast_to_know_frontend/src/app/services/Post.ts
+++ ast_to_know_frontend/src/app/services/Post.ts
@@ -4,5 +4,14 @@ export interface Post {
     created_at: string,
     average_votes: number,
     total_comments: number,
+    user: {name: string},
+    is_voted: boolean
+}
+
+export interface Comment {
+    id: number,
+    comment_at: string,
+    content: string,
+    is_owner: boolean,
     user: {name: string}
 }
